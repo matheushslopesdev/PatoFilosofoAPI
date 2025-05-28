ade23961b860e7aa2c50ba07301e163b5600ad7e
Matheus Lopes <matheushslopes.developer@gmail.com>
2025-05-27 22:48:06 -0300

    Add files via upload

--- assets/nomeDosPatos.js
+++ assets/nomeDosPatos.js
@@ -1,22 +1,22 @@
 module.exports = nomeDosPatos = [
-  "Pato Sócrates",
-  "Platão das Águas Rasas",
-  "Nietzquac",
-  "Aristopato",
-  "Confupato",
-  "Friedrich Quacsche",
-  "Descartesquac",
-  "Lamaquac",
-  "Jean-Quac Sartre",
-  "Paticínio de La Granja",
-  "Quacxime de Mileto",
-  "Pato Epicurioso",
-  "Heguelhinho",
-  "Michel Foucaultic",
-  "Pato Parmênides",
-  "Quactino Gramsci",
-  "Herácliquac",
-  "Pondé de Penas",
-  "Kantando no Lago",
-  "Karl Quacx"
+  "Aristopato",           // Aristóteles + pato lógico demais
+  "Platãoquack",          // Platão + o eco das ideias no lago
+  "Sócrapato",            // Sócrates + chato do tanque
+  "Nietzschpato",         // Nietzsche + pato revoltado
+  "Descartesquack",       // Descartes + penso, logo piu
+  "Kantaganso",           // Kant + limites até pra voar
+  "Confúcioquack",        // Confúcio + confusão no lago
+  "Maquiavelhato",        // Maquiavel + manipulador da lagoa
+  "Freuduck",             // Freud + interpreta seus pesadelos com raposas
+  "Espinozau",            // Espinoza + pato zen forçado
+  "Marxquack",            // Marx + o comunista da lagoa
+  "Sartrequack",          // Sartre + pato existencialista e angustiado
+  "Lao-Tséquack",         // Lao-Tsé + flui como água (literalmente)
+  "Pascálhato",           // Pascal + aposta se vai chover ou não
+  "Hegelganso",           // Hegel + incompreendido até por outros patos
+  "Derriduck",            // Derrida + desconstrói o próprio ninho
+  "Hannah Quackendt",     // Hannah Arendt + contra a banalidade da ração
+  "Foucaultpato",         // Foucault + controla o poder do milho
+  "Simone de Quackvoir",  // Simone de Beauvoir + rebelde e livre
+  "Berquackley",          // Berkeley + só existe se o lago notar
 ];
